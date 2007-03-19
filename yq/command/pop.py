b91ac4f2016220e82de966bfddc9b07ffd797343
James Bowes <jbowes@dangerouslyinc.com>
2007-03-18 22:10:54 -0400

    fill in pop command

--- yq/command/pop.py
+++ yq/command/pop.py
@@ -10,4 +10,56 @@ class Pop(command.Command):
         self.options = options
 
     def do(self, args):
-        pass
+        import os
+        from yum import YumBase
+
+        my_yum = YumBase()
+        series = open("/var/lib/yq/status", 'r')
+        for line in series: pass
+        transaction_name = line.strip()
+        transaction = open(os.path.join("/var/lib/yq/", transaction_name), 'r')
+
+        for line in transaction:
+            line = line.strip()
+            parts = line.split(' ')
+            if parts[0] == '-':
+                fn = my_yum.install
+            elif parts[0] == '+':
+                fn = my_yum.remove
+            else:
+                assert False, "corrupt transaction file"
+
+            if parts[2] == 'None':
+                parts[2] = 0
+            fn(name=parts[1], epoch=parts[2], version=parts[3],
+                    release=parts[4], arch=parts[5])
+
+        dlpkgs = map(lambda x: x.po, filter(lambda txmbr:
+                                            txmbr.ts_state in ("i", "u"),
+                                            my_yum.tsInfo.getMembers()))
+        my_yum.downloadPkgs(dlpkgs)
+
+        my_yum.initActionTs() # make a new, blank ts to populate
+        my_yum.populateTs(keepold=0)
+        my_yum.ts.check() #required for ordering
+        my_yum.ts.order() # order
+
+        # FIXME: is it really sane to use this from here?
+        import sys
+        sys.path.append('/usr/share/yum-cli')
+        import callback
+
+        cb = callback.RPMInstallCallback(output = 0)
+        cb.filelog = True
+        cb.tsInfo = my_yum.tsInfo
+        my_yum.runTransaction(cb)
+
+        #Remove from the series
+        #yes, this will eat files and babies
+        series = open("/var/lib/yq/status", 'r')
+        lines = series.readlines()
+        series.close()
+        series = open("/var/lib/yq/status", 'w')
+        for line in lines[:-1]:
+            series.write(line)
+        series.close()
