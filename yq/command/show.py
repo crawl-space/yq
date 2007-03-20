4f6608f6dfdaa3e0243d6efd8a992a440ab77d39
James Bowes <jbowes@dangerouslyinc.com>
2007-03-19 22:05:21 -0400

    show: support displaying arbitrary transactions

--- yq/command/show.py
+++ yq/command/show.py
@@ -4,25 +4,32 @@ from yq.util import command
 
 class Show(command.Command):
     summary = "print the changes in a transaction"
+    usage = "show [TRANSACTION]"
 
     def handleOptions(self, options):
         self.options = options
 
     def do(self, args):
         import os
- 
-        top_transaction = None
-        status = open("/var/lib/yq/status", 'r')
-        for line in status:
-            top_transaction = line
-        status.close()
 
-        if not top_transaction:
-            print "no applied transactions"    
+        if len(args) == 0:
+            top_transaction = None
+            status = open("/var/lib/yq/status", 'r')
+            for line in status:
+                top_transaction = line
+            status.close()
+
+            if not top_transaction:
+                print "no applied transactions"
+                return 0
+            else:
+                transaction_name = top_transaction.strip()
         else:
-            transaction = open(os.path.join("/var/lib/yq/",
-                top_transaction.strip()), 'r')
+            transaction_name = args[0]
 
-            for line in transaction:
-                line = line.strip()
-                print line
+        transaction = open(os.path.join("/var/lib/yq/",
+            transaction_name), 'r')
+
+        for line in transaction:
+            line = line.strip()
+            print line
