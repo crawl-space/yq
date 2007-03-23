0ac9c935dca4b10932fd100fa147437759fe0f41
James Bowes <jbowes@dangerouslyinc.com>
2007-03-23 13:40:46 -0400

    add stackdir, base, series and status to config
    Signed-off-by: James Bowes <jbowes@dangerouslyinc.com>

--- yq/util/config.py
+++ yq/util/config.py
@@ -1,3 +1,11 @@
 # vi:si:et:sw=4:sts=4:ts=4
 
+import os
+
 VERSION = '0.0.2'
+
+STACKDIR = '/var/lib/yq'
+
+BASE = os.path.join(STACKDIR, 'base')
+SERIES = os.path.join(STACKDIR, 'series')
+STATUS = os.path.join(STACKDIR, 'status')
