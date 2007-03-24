4b1aea5e4dc623131c236e8182aa52ea1447891f
James Bowes <jbowes@dangerouslyinc.com>
2007-03-24 12:32:29 -0400

    push: put yum specific code into a new backend module
    Signed-off-by: James Bowes <jbowes@dangerouslyinc.com>

--- yq/command/push.py
+++ yq/command/push.py
@@ -11,50 +11,11 @@ class Push(command.Command):
         self.options = options
 
     def do(self, args):
-        import os
-        from yum import YumBase
         from yq import stack
-
-        my_yum = YumBase()
+        from yq.backend import yumbackend
 
         transaction_name = stack.next()
-        transaction = open(os.path.join(config.STACKDIR, transaction_name),
-                'r')
-
-        for line in transaction:
-            line = line.strip()
-            parts = line.split(' ')
-            if parts[0] == '-':
-                fn = my_yum.remove
-            elif parts[0] == '+':
-                fn = my_yum.install
-            else:
-                assert False, "corrupt transaction file"
-
-            if parts[2] == 'None':
-                parts[2] = None
-            fn(name=parts[1], epoch=parts[2], version=parts[3],
-                    release=parts[4], arch=parts[5])
-
-        dlpkgs = map(lambda x: x.po, filter(lambda txmbr:
-                                            txmbr.ts_state in ("i", "u"),
-                                            my_yum.tsInfo.getMembers()))
-        my_yum.downloadPkgs(dlpkgs)
-
-        my_yum.initActionTs() # make a new, blank ts to populate
-        my_yum.populateTs(keepold=0)
-        my_yum.ts.check() #required for ordering
-        my_yum.ts.order() # order
-
-        # FIXME: is it really sane to use this from here?
-        import sys
-        sys.path.append('/usr/share/yum-cli')
-        import callback
-
-        cb = callback.RPMInstallCallback(output = 0)
-        cb.filelog = True
-        cb.tsInfo = my_yum.tsInfo
-        my_yum.runTransaction(cb)
+        yumbackend.push(transaction_name)
 
         # add to the status
         status = open(config.STATUS, 'a')
